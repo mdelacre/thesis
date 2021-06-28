cfda33f2a64ed85b27d5307cc328281f5e38f766
Marie Delacre <marie.delacre@ulb.be>
2021-06-28 19:08:51 +0200

    .

--- Code table 1&2 chp5.R
+++ Code table 1&2 chp5.R
@@ -1,30 +1,48 @@
-# Table 1: proportion d'itérations conduisant à NRHO
-# Sans contrainte de puissance
-
-semin <- function(n){
+semin <- function(n,nSim=100000){
 
 meandiff <- seq(.1,.5,.1)
-NRH0 <- NULL
-
+NRH0_1 <- NULL
+NRH0_2 <- NULL
 
 for (j in seq_len(length(meandiff))){
 
   mu1 <- 0
   mu2 <- meandiff[j]
-  pval <- NULL
+  equiv1 <- NULL
+  equiv2 <- NULL
   
-  for (i in seq_len(100000)){
+  for (i in seq_len(nSim)){
     ech1 <- rnorm(n,mu1,1)
     ech2 <- rnorm(n,mu2,1)
     res <- t.test(ech1,ech2,var.equal=T)
-    pval <- c(pval,res$p.value) # p-value
+    
+    # Sans contrainte
+    equivalence1 <- (res$p.value>.05)
+    equiv1 <- c(equiv1,equivalence1)    
+    
+    # Avec contrainte
+    required_power <- .80
+    alpha <- .05
+    theta <- .3 # SESOI (exprimé en différence de moyennes)
+    pow <- pwr.t.test(n=n,sig.level=alpha,
+                      power=required_power,
+                      type="two.sample",
+                      alternative="two.sided")
+    d_min <- pow$d # taille d'effet standardisée qui permet d'atteindre
+                   # la puissance requise
+    S_max <- theta/d_min # d_min <- theta/S_max <--> S_max = theta/d_min
+    S <- sqrt(((n-1)*sd(ech1)^2+(n-1)*sd(ech2)^2)/(2*n-2))
+    
+    equivalence2 <- (res$p.value>.05 & S <= S_max) # Si S < S_max, alors on a assez de puissance
+    equiv2 <- c(equiv2,equivalence2)    
   }
 
-  NRH0 <- c(NRH0,sum(pval>.05)/length(pval))
+  NRH0_1 <- c(NRH0_1,sum(equiv1==TRUE)/nSim)
+  NRH0_2 <- c(NRH0_2,sum(equiv2==TRUE)/nSim)
   
     }
 
- return(round(NRH0,3))
+ return(rbind(round(NRH0_1,3),round(NRH0_2,3)))
 
 }
 
@@ -37,59 +55,12 @@ F<-semin(n=600)
 G<-semin(n=700)
 
 setwd("C:/Users/Admin/Documents/Github projects/thesis/Chapitre 5/Illustration")
-write.table(rbind(A,B,C,D,E,F,G),"table1.txt",sep=";",dec=",")
+# Table 1: proportion d'itérations conduisant à NRHO
+# sans contrainte
+write.table(rbind(A[1,],B[1,],C[1,],D[1,],E[1,],F[1,],G[1,]),"table1.txt",sep=";",dec=",")
 
 # Table 2: proportion d'itérations conduisant à NRHO
-# Avec contrainte de puissance: puissance d'au moins 80% pour détecter
-# Une différence de .1
+# avec contrainte
+write.table(rbind(A[2,],B[2,],C[2,],D[2,],E[2,],F[2,],G[2,]),"table2.txt",sep=";",dec=",")
 
-semin2 <- function(n){
-  
-  meandiff <- seq(.1,.5,.1)
-  NRH0 <- NULL
-  for (j in seq_len(length(meandiff))){
-    
-    mu1 <- 0
-    mu2 <- meandiff[j]
-    equiv <- NULL
-    
-    for (i in seq_len(100000)){
-      ech1 <- rnorm(n,mu1,1)
-      ech2 <- rnorm(n,mu2,1)
-      res <- t.test(ech1,ech2,var.equal=T)
-      pval <- res$p.value # p-value
-      S <- sqrt(((n-1)*sd(ech1)^2+(n-1)*sd(ech2)^2)/(2*n-2))
-
-      # Estimation de la puissance      
-      required_power <- .80
-      alpha <- .05
-      theta <- .3 # SESOI (exprimé en différence de moyennes)
-      res <- pwr.t.test(n=n,sig.level=alpha,
-                        power=required_power,
-                        type="two.sample",
-                        alternative="two.sided")
-      d_min <- res$d # taille d'effet standardisée qui permet d'atteindre la
-                     # puissance requise
-      S_max <- theta/d_min # d_min <- theta/S_max <--> S_max = theta/d_min
-      equivalence <- (pval>.05 & S <= S_max)
-      equiv <- c(equiv,equivalence) 
-    }
-
-    NRH0 <- c(NRH0,sum(equiv==TRUE)/100000)
-    
-  }
-  
-  return(NRH0)
-  
-}
-
-A<-semin2(n=100)
-B<-semin2(n=200)
-C<-semin2(n=300)
-D<-semin2(n=400)
-E<-semin2(n=500)
-F<-semin2(n=600)
-G<-semin2(n=700)
-
-write.table(rbind(A,B,C,D,E,F,G),"table2.txt",sep=";",dec=",")
 
