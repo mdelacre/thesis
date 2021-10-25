c79371ce9b6f612d9dfeb1105ed6e66514e2fbbd
Marie Delacre <marie.delacre@ulb.ac.be>
2021-10-25 17:08:28 +0200

    .

--- Préparation défense thèse/Yuen_calculations.R
+++ Préparation défense thèse/Yuen_calculations.R
@@ -4,7 +4,7 @@ A<-c(1,3,5,6,7,10,11,12,75,75)
 B<-c(1,4,5,8,19,20,21,21,23,24)
 
 ### Trimming
-TR=0
+TR=0.20
 
 ### Série utilisée pour calculer la moyenne trimmée:
 ### suppression des 20% des scores les plus faibles
